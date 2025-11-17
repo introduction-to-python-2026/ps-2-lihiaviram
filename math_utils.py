e6427e8c757bafc4843cb641657bc6310acc0584
lihiaviram <lihiviram@gmail.com>
2025-11-17 14:27:23 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -12,6 +12,7 @@ def find_mean(num1, num2, num3):
      
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    sum=(num1-mean)**2+(num2-mean)**2+(num3-mean)**2
-    sum=sum/3
-    return sum**0.5
+    variance = ((num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean)**2) / 3
+    std = variance ** 0.5
+    return mean, std
+
