4a3eec1686023f942acf7ef45737a6fe1fc3216b
lihiaviram <lihiviram@gmail.com>
2025-11-17 13:58:56 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,13 +1,13 @@
 def find_max_number(num1, num2, num3):
-   if num1>num2 :
-       if num1>num3:
-           return num1
-       else:
-            return num3
-     if num2>num3:
-        return num2
-     else:
+   if num1>num2:
+      if num1>num3:
+         return num1
+      else:
            return num3
+   elif num2>num3:
+        return num2
+   else:
+        return num3
 
 def find_mean(num1, num2, num3):
     sum=(num1+num2+num3)/3
